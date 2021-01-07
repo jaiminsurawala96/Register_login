8912958eed4b7474b09f90dd30441de49db6fbd8
Jaimin Surawala <jaimin@Jaimins-MacBook-Pro.local>
2021-01-06 22:06:26 -0800

    Register_login_app_files

---
+++ register_login/register/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [path('',views.home),
+               path('register/',views.reg),
+               path('loggedin/',views.loggedin),
+               ]
